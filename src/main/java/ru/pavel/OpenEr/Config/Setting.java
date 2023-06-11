88102ae07f77632091b908dd712d5285302a1a20
Mikhno Pavel <p.yu.mihno@inbox.ru>
2023-06-11 20:24:39 +0300

    Обновил класс Setting

--- src/main/java/ru/pavel/OpenEr/Config/Setting.java
+++ src/main/java/ru/pavel/OpenEr/Config/Setting.java
@@ -1,8 +1,12 @@
 package ru.pavel.OpenEr.Config;
 
+import java.io.File;
+
 public class Setting {
-    private int activationKeyVirtualCode;
     private static Setting setting;
+    private int activationKeyVirtualCode;
+    private File configFile;
+    private File shortcutsFile;
 
     private Setting(){
     }
@@ -17,6 +21,21 @@ public class Setting {
         }
         return setting;
     }
+
+    public File getShortcutsFile(){
+        return this.configFile;
+    }
+    public void setShortcutsFile(File shortcutsFile){
+        this.shortcutsFile = shortcutsFile;
+    }
+
+    public File getConfigFile(){
+        return this.configFile;
+    }
+    public void setConfigFile(File configFile){
+        this.configFile = configFile;
+    }
+
     public int getActivationKeyVirtualCode(){
         return this.activationKeyVirtualCode;
     }
